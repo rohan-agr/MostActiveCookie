0864d9ee3337cdf33963f945bbfe9472fa2f064d
Rohan Agrawal <rohanagr@umich.edu>
2023-01-13 00:11:24 -0500

    Using unordered_map library

--- most_active_cookie.cpp
+++ most_active_cookie.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <getopt.h>
-#include <string>
+#include <string.h>
 #include <fstream>
 #include <sstream>
+#include <unordered_map>
 
 using namespace std;
 
@@ -51,12 +52,14 @@ int main (int argc, char** argv)
     string fname = theDriver.get_filename(argc, argv);
     string cookieDate = theDriver.get_options(argc, argv);
 
+    //Opens file
     ifstream fin(fname);
     if (fin.is_open() == 0)
     {
         cout << "Error: file could not be opened.";
         return 1;
     }
+
     string line;
 
     //Take in the top of the input file (cookies, timestamp)
@@ -65,21 +68,51 @@ int main (int argc, char** argv)
     //Main solution
     /*
         Takes in the input csv file, line by line. 
-            - Does not do anything in the case that the cookie log date is more recent than the argument date
+            - Does not do anything in the case that the cookie log date is 
+              more recent than the argument date
             - Breaks from the loop if the cookie log date is older 
-
+            - If cookie is from the correct date, adds/increments frequency
+              counter in a hash map from cookie id to frequency. Augments the 
+              max_frequency variable as necessary
+            - Finally, iterates through unordered map, printing the IDs of the
+              most accessed cookies
     */
+
+   //Boolean variable to record if we have accessed records from the correct date
+   //If we have, and then we don't get a match between cookieDate and date,
+   //we have passed the range of cookies and can break out of the loop.
+    bool dateRange = 0; 
+
+    unordered_map<string, int> cookieHash;
+    int max_frequency = 0;
+
     while(getline(fin, line))
     {
-        stringstream ss(line);
-        string id;
-        string date;
-        string time;
+        string id = line.substr(0, 16);
+        string date = line.substr(17, 10);
 
-        ss >> id >> date >> time;
+        if ((cookieDate.compare(date)) != 0)
+        {
+            if (dateRange == 1){break;}
+        }
+        else
+        {
+            dateRange = 1;
+            cookieHash[id]++;
+            if (cookieHash[id] > max_frequency){max_frequency = cookieHash[id];}
+        }
 
-        cout << id << date << time << endl;
     }
+
+    unordered_map<string, int>::iterator it = cookieHash.begin();
+     
+    //Iterating over the map using iterator until map end.
+    while (it != cookieHash.end())
+    {
+        if (it->second == max_frequency){cout << it->first << '\n';}
+        it++;
+    }
+    return 0;
 
     fin.close();
     return 1;
